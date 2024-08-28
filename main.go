335bbbf97c0d977d32e65b69f92511d164e15a41
acaba <alejandro.caballero@vivint.com>
2024-08-28 12:05:39 -0400

    First commit
    bare bone subscribe and unsubscribe

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Message struct {
+	Topic   string
+	Payload interface{}
+}
+
+type Subscriber struct {
+	Channel     chan interface{}
+	Unsubscribe chan bool
+}
+
+type Broker struct {
+	subscribers map[string][]*Subscriber
+	mutex       sync.Mutex
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		subscribers: make(map[string][]*Subscriber),
+	}
+}
+
+func (b *Broker) Subscribe(topic string) *Subscriber {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	subscriber := &Subscriber{
+		Channel:     make(chan interface{}),
+		Unsubscribe: make(chan bool),
+	}
+	b.subscribers[topic] = append(b.subscribers[topic], subscriber)
+
+	return subscriber
+}
+
+func (b *Broker) Unsubscribe(topic string, subscriber *Subscriber) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	if subscribers, found := b.subscribers[topic]; found {
+		for i, sub := range subscribers {
+			if sub == subscriber {
+				close(sub.Channel)
+				b.subscribers[topic] = append(subscribers[:i], subscribers[i+1:]...)
+				return
+			}
+		}
+	}
+}
+
+func (b *Broker) Publish(topic string, payload interface{}) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	if subscribers, found := b.subscribers[topic]; found {
+		for _, sub := range subscribers {
+			select {
+			case sub.Channel <- payload:
+			case <-time.After(time.Second):
+				fmt.Println("Subscribers slow. Unsubscribing from topic: s\n", topic)
+				b.Unsubscribe(topic, sub)
+			}
+		}
+	}
+
+}
+
+func main() {
+
+	broker := NewBroker()
+
+	subscriber := broker.Subscribe("example_topic")
+	go func() {
+		for {
+			select {
+			case msg, ok := <-subscriber.Channel:
+				if !ok {
+					fmt.Println("Subscriber channel closed.")
+					return
+				}
+				fmt.Printf("Received: %v\n", msg)
+			case <-subscriber.Unsubscribe:
+				fmt.Println("Unsubscribed")
+				return
+			}
+		}
+	}()
+
+	broker.Publish("example_topic", "Hello, World!")
+	broker.Publish("example_topic", "This is a test message.")
+
+	time.Sleep(2 * time.Second)
+	broker.Unsubscribe("example_topic", subscriber)
+
+	broker.Publish("example_topic", "This message won't be received.")
+
+	time.Sleep(time.Second)
+}
